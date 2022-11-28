3eae297be55183edbd982eb3bbbc0315112e30eb
Robert Jensen <robert@robert-jensen.dk>
2022-11-28 22:27:27 +0100

    Added cleanup script

---
+++ 08-cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+source 00-functions.sh
+
+function delete_clusters() {
+    info "Delete workload clusters in Supervisor Cluster $1"
+
+    kubectx $1
+    
+    kubectl delete -f clusters/view-cluster.yaml -n $SV_NS_NON_PROD --wait=false
+    kubectl delete -f clusters/build-cluster.yaml -n $SV_NS_NON_PROD --wait=false
+    kubectl delete -f clusters/run-cluster.yaml -n $SV_NS_PROD --wait=false
+}
+
+loginToSupervisor $KUBECTX_SV_CLUSTER
+
+delete_clusters $KUBECTX_SV_CLUSTER
+
+success "Clusters Deleted!"
